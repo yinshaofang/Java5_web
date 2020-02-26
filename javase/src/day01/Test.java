8684afa5795a494cc179b15bf6e329690f27da02
laowang1 <333@qq.com>
2020-02-26 10:51:07 +0800

    第四次修改

--- javase/src/day01/Test.java
+++ javase/src/day01/Test.java
@@ -5,7 +5,7 @@ import java.io.File;
 public class Test {
     // psvm
     public static void main(String[] args) {
-        //
+        //sout
         System.out.println("hello! Idea!!");
         File f =new File("");
 
